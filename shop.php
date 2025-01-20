ca609ead923eec222026812bc39f298ee7db94f7
djalabz <djalabz@gmail.com>
2025-01-20 14:27:07 +0100

    :lipstick: Stylized products

--- shop.php
+++ shop.php
@@ -10,15 +10,19 @@ include "config/curl.php";
 </h1>
 
 
-    <section class="shop">
+    <section class="grid grid-cols-4 mt-12 gap-x-6 gap-y-6 shop">
 
+        <!-- On récupère la variable $decoded (car le fichier curl est inclus en haut)
+        Cette variable est un tableau on peut donc utiliser foreach pour le parcourir -->
         <?php foreach($decoded as $item) : ?>
 
+            <!-- La div qui va afficher les items ed notre store -->
             <div class="product">
                 <img src="<?= $item->images[0] ?>">
                 <h3><?= $item->title ?></h3> 
-                <h3><?= $item->price ?></h3>
-                <h4><?= $item->description ?></h4>
+                <h3><?= $item->price . " $"?></h3>
+                <h4><?= substr($item->description, 0, 120) . " ..." ?></h4>
+                <button class="rounded border border-black px-2 py-1">Ajouter au panier</button>
             </div>
 
 
